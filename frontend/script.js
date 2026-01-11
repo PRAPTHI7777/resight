2c73c602d1aca1707681ab49b3797c90f55e4b5a
Prapthi <pjprapthi@gmail.com>
2026-01-12 00:03:32 +0530

    fetchdatafromjson

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,46 @@
-//Fetch data from local JSON file and render it to HTML document by using Vanilla JavaScript 
-import data from "./data.json" assert { type: "json" };
-console.log(data);
+//Fetch data from local JSON file and render it to HTML document by using Vanilla JavaScript
+alert('JavaScript is linked successfully!'); 
+const container=document.querySelector('.contain');
+fetch('./data.json')
+    .then(response => response.json())
+    .then(data => {
+        renderCharacters(data);
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+
+function renderCharacters(data){
+    container.innerHTML='';
+    data.forEach(item => {
+        const itemDiv=document.createElement('div');
+        const pict=document.createElement('div');
+        const verticle=document.createElement('div');
+        const titleDiv=document.createElement('div');
+        const authorDiv=document.createElement('div');
+        const summaryDiv=document.createElement('div');
+        const dateDiv=document.createElement('div');
+        const categoryDiv=document.createElement('div');
+        itemDiv.className='container';
+        pict.className='picture';
+        verticle.className='verticalcontain';
+        titleDiv.className='title';
+        authorDiv.className='authors';
+        summaryDiv.className='summary';
+        dateDiv.className='date';
+        categoryDiv.className='category';
+        titleDiv.textContent=item.title;
+        authorDiv.textContent='- '+item.authors.join(', ');
+        summaryDiv.textContent=item.description;
+        dateDiv.textContent=item.date;
+        categoryDiv.textContent=item.category;
+        itemDiv.appendChild(pict);
+        itemDiv.appendChild(verticle);
+        verticle.appendChild(titleDiv);
+        verticle.appendChild(authorDiv);
+        verticle.appendChild(summaryDiv);
+        verticle.appendChild(dateDiv);
+        dateDiv.appendChild(categoryDiv);
+        container.appendChild(itemDiv);
+
+});
+}
